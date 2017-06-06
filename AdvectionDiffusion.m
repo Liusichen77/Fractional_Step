16f5360352a45045bbcacb903c303c91296722f2
Zachary <zackbarry13@gmail.com>
2017-06-05 21:28:34 -0700

    Updated F and A so that the Dirichlet condition at u=0 is included in the matrix calculation.  Saving so I can test reordering of scheme that follows Peszynska's fd1d_heat.m

--- AdvectionDiffusion.m
+++ AdvectionDiffusion.m
@@ -9,17 +9,18 @@
 % advection part of this solver, and Two_Point_BVP_Centered_Difference.m
 % for the diffusion.
 
-a=0.2;  % advection coefficient
+a=0.0475;  % advection coefficient
 b=1;  % diffusion coefficient
 
-I=@(y) -(y-0.7808).^2+0.6096;  % u(x,0)
+%I=@(y) -(y-0.7808).^2+0.6096;  % example u(x,0) (didn't yield good results
+                                % for the first version of this code
 
-m=40;   % # spatial grid points
+m=100;   % # spatial grid points
 xend=1;    % final x value
 h=xend/m;   % step size
 x=[0:h:xend];   % x grid
 
-n=20;
+n=1000;
 tend=2;
 k=tend/n;
 t=[0:k:tend];
@@ -31,38 +32,39 @@ end
 
 % Setup Centered Difference method for Diffusion
 
-A=zeros(m,m);   % Matrix for numerical scheme.
-A(1,1)=2;  % Columns 1 and m done manually because they do not
-A(1,2)=-1;   %   include both the lower and upper diagonals.
-A(m,m)=h;  
+A=sparse(m,m);   % Sparse uses less memory than zeros
+A(1,1)=1;  % Columns 1 and m done manually because they do not
+A(1,2)=0;   %   include both the lower and upper diagonals.
+A(m,m)=h;   % FE approximation of derivative for u'(1)=0 condition
 A(m,m-1)=-h;
 for i=2:m-1; 
-    A(i,i)=2;  % main diagonal is -2
-    A(i,i-1)=-1; % upper diagonal is 1
-    A(i,i+1)=-1; % lower diagonal is 1
+    A(i,i)=2;  % main diagonal is 2
+    A(i,i-1)=-1; % upper diagonal is -1
+    A(i,i+1)=-1; % lower diagonal is -1
 end
-invA=inv(A);
 
 % Advection, then Diffusion
 
-u1=I(x);  % initial data u(x,0)
+u1=zeros(1,length(x));
+u1(round(0.1*(length(x)/xend)+1):round(0.3*(length(x)/xend)))=1;
 
 close all
 figure
 plot(x,u1)
 hold on
-for i=1:n
+for i=1:3
 	u0=u1;	% update (i-1)k solution vector
 	u1(2:m)=u0(2:m)-(a*k/h)*(u0(2:m)-u0(1:m-1)); % upwind advection for a>0
 	u1(1)=0; 	% Dirichlet BC at x=0
     u1(m+1)=u1(m);  % Neumann BC at x=1
-    F=zeros(1,m);
-    for i=2:m
+    F=zeros(m,1);
+    for i=3:m
         F(i-1)=(u1(i)-u0(i))/k;   % f(x(i))=u_t(x(i))
     end
-    F(m)=0;
-    u1(2:m+1)=(2.1*h^2.*invA*F')'; % centered difference for diffusion
+    F(m)=0; % Neumann BC
+    F=-F;
+    u1(2:m+1)=1e2*(h^2.*(A\F))'; % centered difference for diffusion
     plot(x,u1)
-    pause(0.1)
+    pause(0.01)
 end
 hold off
